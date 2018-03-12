fe1fa0587c97ac8fdb0b9ed443bde09614ec47ae
Frank Germano <fgermano@earthlink.net>
2018-03-12 09:52:54 -0600

    fgj misc

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -97,7 +97,6 @@ class AuthenticationController < ApplicationController
   end
 
   def send_token_via_email(user, token)
-puts token
     from = 'fgermano@earthlink.net' # In production, this would be admin@emrbear.com, etc.
     to = [user.email]
     subject = 'Emrbear Accounts Login Token Validation'
